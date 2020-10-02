f6d57efca9a50e3abe0c3f1117891fa1a3e1979b
Christina Gerstner <christinalgerstner@gmail.com>
2020-10-01 23:25:18 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipe-search';
+  title = 'Recipes';
+  food = '';
+  hits = [];
+
+  constructor(private _http: HttpClient) {}
+
+  submit() {
+    let url: string;
+    url = "https://api.edamam.com/search";
+    url += "?q=" + this.food;
+    url += "&app_id=d3d557b4";
+    url += "&app_key=031c118d4e74959a469b02eb8cc24ca4";
+    url += "&from=0";
+    url += "&to=5";
+
+    //console.log(url);
+
+    this._http.get(url)
+      .subscribe((data: any) => {
+        this.hits = data.hits;
+        console.log(this.hits);
+        //console.log(data);
+      });
+  }
 }
